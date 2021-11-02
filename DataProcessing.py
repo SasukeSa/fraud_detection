1290010b0c8f46f2277d36be85b978e4bc27e736
sasukesa <abhisheksa.wlf@gmail.com>
2021-11-02 16:06:01 +0530

    Added sum method to the python file

--- DataProcessing.py
+++ DataProcessing.py
@@ -1,1 +1,12 @@
 print("Adding all the data processing code related to fraud detection")
+
+a = 5
+b = 7
+def sum(a,b):
+    c = a+b
+    return a #mistake done intentionally, should be c only
+
+#Call the method sum with two variables
+print(sum(a,b))
+
+print('Added new code for the sum')
